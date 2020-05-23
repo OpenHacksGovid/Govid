9e782118fa6756e23b9bd2e49fc5edc86d7646e3
ShayaanAkbar <shayaanakbar1106@gmail.com>
2020-05-23 17:08:20 -0500

    Added About me and Footer

--- my-app/src/components/Navigation/About.js
+++ my-app/src/components/Navigation/About.js
@@ -1,3 +1,19 @@
 import React from 'react';
-const About = () => <h1>This is the About page</h1>;
+
+function About() {
+    return (
+        <div>
+            <h1>Our Mission</h1>
+            <p>To provide the most data for Covid & Governors actions idk what else to say someone help me </p>
+
+            <h2>About</h2>
+            <p>We created Govid because we kept seeing articles about what cities and counties were doing to implemet proper steps into opening up and safety
+                regulations for everyone living in the area and we wanted to make things simple for the next person that was searching! Here at Govid you can see who
+                is in your House of Rep as well as your governor, a link to their website/Twitter and websites that can give you more information
+            </p>
+        </div>
+    )
+}
+
+
 export default About;
