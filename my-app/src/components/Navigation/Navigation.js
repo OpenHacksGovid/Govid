fb15a46eec8d8b280f52c7212e1a5509030e2d09
ShayaanAkbar <shayaanakbar1106@gmail.com>
2020-05-23 14:00:49 -0500

    Fixed JS files and added components in Nav

--- my-app/src/components/Navigation/Navigation.js
+++ my-app/src/components/Navigation/Navigation.js
@@ -7,12 +7,8 @@ class Nav extends React.Component {
             <Router>
             <div className="container">
                 <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
                 </ul>
 
             <Route path="/" exact component={Home} />
