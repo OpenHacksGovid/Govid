b5bb41f83cc77cb13238d0a59150f2009749eac8
ShayaanAkbar <shayaanakbar1106@gmail.com>
2020-05-23 15:56:02 -0500

    Fixed app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './App.css';
 
+
+
 import Body from './components/Body/Body';
 import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation/Navigation";
-
 function App() {
-  return (
-    <div className="App">
-     <div>
-        <Navigation />
-        <Header />
-        <Body />
-     </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>
+                <Navigation />
+                <Header />
+                <Body />
+            </div>
+        </div>
+    );
 }
-
 export default App;
