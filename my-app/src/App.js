8e43ff77d7a06f89d33a3f47af5def75a8aee41c
ShayaanAkbar <shayaanakbar1106@gmail.com>
2020-05-23 16:01:55 -0500

    Finally fixed app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,6 @@ import Body from './components/Body/Body';
 import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation/Navigation";
 function App() {
-
     return (
         <div className="App">
             <div>
@@ -14,16 +13,5 @@ function App() {
             </div>
         </div>
     );
-  return (
-    <div className="App">
-     <div>
-        <Header/>
-        <Map/>
-        <Navigation />
-        <Header />
-        <Body />
-     </div>
-    </div>
-  );
 }
 export default App;
