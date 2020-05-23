b273ab5a71a1fe1cf9e37f90b76f0f47f036d4bb
Malcolm Lynch <milynch1320@gmail.com>
2020-05-23 18:27:27 -0500

    removed extra Header import

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 //import Body from './components/Body/Body';
 import Header from "./components/Header/Header";
 //import Navigation from "./components/Navigation/Navigation";
-import Header from "./components/Header/Header";
 import Footer from './components/Footer/Footer';
 
 function App() {
