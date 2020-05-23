8b951422a9cbab8709aada97a52a1266865a1d41
Malcolm Lynch <milynch1320@gmail.com>
2020-05-23 16:13:16 -0500

    Changed location of map to Home.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import './App.css';
-import Body from './components/Body/Body';
+
+//import Body from './components/Body/Body';
 import Header from "./components/Header/Header";
-import Navigation from "./components/Navigation/Navigation";
+//import Navigation from "./components/Navigation/Navigation";
+import Map from "./components/Map/Map"; 
+
 function App() {
-  return (
-    <div className="App">
-     <div>
-        <Header/>
-        <Map/>
-        <Navigation />
-        <Header />
-        <Body />
-     </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>
+                <Header />
+            </div>
+        </div>
+    );
 }
 export default App;
