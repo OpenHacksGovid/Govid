5599aadbbae2564e7f282806d7c3fef591da51e1
ShayaanAkbar <shayaanakbar1106@gmail.com>
2020-05-23 19:31:34 -0500

    Correct file

--- my-app/src/components/Popup/Popup.js
+++ my-app/src/components/Popup/Popup.js
@@ -1,39 +1,11 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import React, { useState } from 'react';
-=======
 import React from 'react';
->>>>>>> 6edb4e380434a8071523342ba1eb9059a22be7a9
-=======
-import React from 'react';
->>>>>>> c82d401e7dcefc4dac3dbb145c19221ab0a871f2
-
-function Popup() {
-    return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <div className="App">
-            <button
-                onMouseEnter={() => setIsShown(true)}
-                onMouseLeave={() => setIsShown(false)}>
-                Hover over me!
-            </button>
-        </div>
-    );
+class Popup extends React.Component {
+    render() {
+        return (
+            <div className="App" style={{background: "white"}}>
+                <h1>State: {this.props.data}</h1>
+            </div>
+        );
+    }
 }
-
 export default Popup;
-=======
-=======
->>>>>>> c82d401e7dcefc4dac3dbb145c19221ab0a871f2
-        <div className="App" style={{background: "white"}}>
-            <h1>Popup text</h1>
-        </div>
-    );
-}
-<<<<<<< HEAD
-export default Popup;
->>>>>>> 6edb4e380434a8071523342ba1eb9059a22be7a9
-=======
-export default Popup;
->>>>>>> c82d401e7dcefc4dac3dbb145c19221ab0a871f2
