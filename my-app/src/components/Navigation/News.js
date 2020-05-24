86f1600972d6488d8ca29feab52ae130b3a206c5
ShayaanAkbar <shayaanakbar1106@gmail.com>
2020-05-24 07:42:11 -0500

    News changes dont commit yet

--- my-app/src/components/Navigation/News.js
+++ my-app/src/components/Navigation/News.js
@@ -1,12 +1,8 @@
-import React from 'react';
-
-function About() {
-    return (
-        <div>
-          
-        </div>
-    )
-}
-
-
-export default About;
+// import React from 'react';
+//
+// function News() {
+//
+// }
+//
+//
+// export default News;
