4c1c6f2fb0ea8a5f7214f47368e48f141af2b524
Malcolm Lynch <milynch1320@gmail.com>
2020-05-24 08:48:25 -0500

    Mapped through news data

--- my-app/src/components/Navigation/News.js
+++ my-app/src/components/Navigation/News.js
@@ -4,37 +4,32 @@ class News extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            title: '',
-            url: '',
-            published: ''
-           
+            articles: [], 
         }
     }
 
-    componentDidMount() {
-        this.getNews(); 
+    async componentDidMount() {
+        const url = "https://covid19-us-api.herokuapp.com/news";
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({articles: data.message})
+        console.log(data.message)
     }
 
-    getNews() {
-            const url = "https://covid19-us-api.herokuapp.com/news"
-            fetch(url)
-                .then(res => res.json())
-                .then(res => this.setState({ title: res.message[0].data,
-                    url: res.message[0].data,
-                    published: res.message[0].data}))
-                .catch(err => console.log(err));
-    }
-    
     render() {
-        const { title, url, published } = this.state;
+        this.componentDidMount();
         return (
         <div>
-            <p>Hello</p>
-            <p>{title}</p>
-            <p>{url}</p>
-            <p>{published}</p>
+            {this.state.articles.map(article => (
+                <div key={article.id}>
+                    <p>{article.title}</p>
+                    <a href={article.url} target="_blank" 
+                    rel="noopener noreferrer">{article.url}</a>
+                    <p>{article.published}</p> 
+                </div>
+            ))} 
          </div>
-        )
+        );
     }
 }
 
